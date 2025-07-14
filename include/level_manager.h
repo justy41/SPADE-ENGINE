ae2a936752291f005244d8eeac859838fbbba9b0
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2025-07-15 00:07:32 +0300

    Added level difficulty sprites

---
+++ include/level_manager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include "spade.h"
+
+class LevelManager : public Object
+{
+private:
+    
+public:
+    
+    
+    LevelManager();
+    void create() override;
+    void update(float dt) override;
+    void draw() override;
+};
---
+++ src/level_manager.cpp
@@ -0,0 +1,17 @@
+#include "level_manager.h"
+
+LevelManager::LevelManager() : Object(0, 0) {
+    
+}
+
+void LevelManager::create() {
+    
+}
+
+void LevelManager::update(float dt) {
+    
+}
+
+void LevelManager::draw() {
+    
+}
